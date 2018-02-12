af72a3b65431e542853d4b9a9d8096b1b423e929
Nathan <nathan1hughes@gmail.com>
2018-02-12 18:41:43 +0000

    Made into a nice library for use with the GUI

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='CT_Analysing_Library',
+      version='0.1',
+      description='Library used for CT grain analysis at the NPPC',
+      url='https://github.com/SirSharpest/CT_Analysing_Library',
+      author='Nathan Hughes',
+      author_email='nathan1hughes@gmail.com',
+      license='MIT',
+      packages=find_packages('ct_analysing_library'),
+      install_requires=['pandas',
+                        'numpy',
+                        'matplotlib',
+                        'seaborn',
+                        'scipy'],
+      zip_safe=True)
