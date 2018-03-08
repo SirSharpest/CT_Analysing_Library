d8184f84232a6516b92d053a9fa4b562354326e1
Nathan <nathan1hughes@gmail.com>
2018-03-08 13:11:56 +0000

    Non-polymorphic function removal

--- ct_analysing_library/graphing.py
+++ ct_analysing_library/graphing.py
@@ -28,62 +28,6 @@ class InvalidPlot(Error):
     pass
 
 
-def percentile_grid(dataframe, attributes):
-    fig, axes = plt.subplots(2, 4, sharex=True)
-    for idx, att in enumerate(attributes):
-        x = idx // 4
-        y = idx % 4
-        percentiles = [np.percentile(dataframe[att], i)
-                       for i in range(1, 100)]
-        axes[x, y].bar(np.arange(1, 100), percentiles)
-        axes[x, y].set_title(att)
-
-        if x < 0:
-            axes[x, y].set_xlabel('')
-        else:
-            axes[x, y].set_xlabel('Percentile')
-
-        if y == 0 or y == 4:
-            axes[x, y].set_ylabel('Value of {0}'.format(att))
-        else:
-            axes[x, y].set_ylabel('')
-
-    return (fig, axes)
-
-
-def qq_grid(dataframe, attributes):
-    fig, axes = plt.subplots(2, 4, sharex=True, sharey=True)
-    for idx, att in enumerate(attributes):
-        x = idx // 4
-        y = idx % 4
-
-        # Convert everything to log10 for this
-        qqplot(np.log10(dataframe[att]), plot=axes[x, y])
-        axes[x, y].set_title(att)
-
-        if x < 0:
-            axes[x, y].set_xlabel('')
-
-        if y == 0 or y == 4:
-            axes[x, y].set_ylabel('Ordered Values')
-        else:
-            axes[x, y].set_ylabel('')
-
-        axes[x, y].get_lines()[0].set_marker('o')
-        axes[x, y].get_lines()[0].set_markerfacecolor('white')
-        w, p = normaltest(dataframe[att])
-
-        if p < 1e-2:
-            p = '<0.01'
-        elif p < 5e-1:
-            p = '<0.05'
-        else:
-            p = '>0.05'
-
-        axes[x, y].text(1, -6, r'$P${0}'.format(p))
-    return (fig, axes)
-
-
 def plot_boxplot(data, attribute, x_var='Sample name', hue='None'):
     """
     This should just create a single boxplot and return the figure
